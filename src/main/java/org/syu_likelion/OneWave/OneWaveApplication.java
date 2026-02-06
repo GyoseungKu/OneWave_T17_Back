d9554c800dec80c8f0491d3d089197d70e037c3a
Gyoseung Ku <106974512+GyoseungKu@users.noreply.github.com>
2026-02-06 15:45:13 +0900

    초기파일 업로드

---
+++ src/main/java/org/syu_likelion/OneWave/OneWaveApplication.java
@@ -0,0 +1,13 @@
+package org.syu_likelion.OneWave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/syu_likelion/OneWave/OneWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.syu_likelion.OneWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
